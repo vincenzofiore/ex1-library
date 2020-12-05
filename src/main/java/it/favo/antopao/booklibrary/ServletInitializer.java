b4af709c4e01f7d13fcf4732cd568e5fbed4972f
Vincenzo Fiore <vincenzo.fiore@afea.eu>
2020-12-05 12:25:13 +0100

    Added sprinboot to project closes #1

---
+++ src/main/java/it/favo/antopao/booklibrary/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.favo.antopao.booklibrary;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookLibraryApplication.class);
+	}
+
+}
