ed78780b9bb2d13b2f6af0ff6687e25d234202d6
4n70 <35333854+4n7o@users.noreply.github.com>
2020-12-18 12:16:48 +0100

    Changes naming

---
+++ src/main/java/it/favo/antopao/booklibrary/entity/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.favo.antopao.booklibrary.entity;
+
+/**
+ * @author A. Favorito on 18-12-2020
+ *
+ * 
+ */
+public class Person {
+
+	/**
+	 * 
+	 */
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
