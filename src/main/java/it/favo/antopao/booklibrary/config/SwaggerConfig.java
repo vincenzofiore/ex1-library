66aabf5e8c9da51eff29208bf30dedc3a818575c
Vincenzo Fiore <vincenzo.fiore@afea.eu>
2021-01-02 08:05:01 +0100

    BL-03 closes #3

--- src/main/java/it/favo/antopao/booklibrary/config/SwaggerConfig.java
+++ src/main/java/it/favo/antopao/booklibrary/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package it.favo.antopao.booklibrary.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +24,20 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build()
-				.pathMapping("/");
+				.pathMapping("/").apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		Contact contact = new Contact("Antonella Favorito", "", "antonellafavorito@gmail.com");
+
+		return new ApiInfo("Book Library", //
+				"Sparagn&Cumparisc", //
+				"1.0", //
+				"Terms of Service: tbd", //
+				contact, //
+				"Apache License Version 2.0", //
+				"https://www.apache.org/licenses/LICENSE-2.0", //
+				new ArrayList<>());
 	}
 
 }
