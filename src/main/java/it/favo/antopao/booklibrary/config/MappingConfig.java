0617c7f16ff088580e7962f97f63e3ad7ff62066
Vincenzo Fiore <vincenzo.fiore@afea.eu>
2021-01-02 19:35:51 +0100

    BL-11 dto to entity mapping closes #11
    used modelmapper instead of mapstruct

---
+++ src/main/java/it/favo/antopao/booklibrary/config/MappingConfig.java
@@ -0,0 +1,15 @@
+package it.favo.antopao.booklibrary.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MappingConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
