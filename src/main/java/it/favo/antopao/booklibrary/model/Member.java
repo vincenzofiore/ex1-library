c3d9eb4fed304beb230fc21593994a4cdaf96f75
Paolafavorito <paola.favorito@gmail.com>
2021-01-12 19:05:51 +0100

    It's 19:05

--- src/main/java/it/favo/antopao/booklibrary/model/Member.java
+++ src/main/java/it/favo/antopao/booklibrary/model/Member.java
@@ -1,9 +1,13 @@
 package it.favo.antopao.booklibrary.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
 
 @Entity
 
@@ -12,6 +16,14 @@ public class Member {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@OneToOne
+	private Long applicationUserId;
+	private String name;
+	private String surname;
+	private Date insDate;
+	private String telNumber;
+	private String email;
+	private String address;
+	@Lob
+	private String note;
 	
-
 }
