2e5bf2f45e6100605209f811a3aecae65b5ac256
Paolafavorito <paola.favorito@gmail.com>
2021-01-12 19:17:07 +0100

    19:17

--- src/main/java/it/favo/antopao/booklibrary/model/Member.java
+++ src/main/java/it/favo/antopao/booklibrary/model/Member.java
@@ -16,7 +16,7 @@ public class Member {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@OneToOne
-	private Long applicationUserId;
+	private User applicationUserId;
 	private String name;
 	private String surname;
 	private Date insDate;
@@ -25,5 +25,116 @@ public class Member {
 	private String address;
 	@Lob
 	private String note;
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the applicationUserId
+	 */
+	public User getApplicationUserId() {
+		return applicationUserId;
+	}
+	/**
+	 * @param applicationUserId the applicationUserId to set
+	 */
+	public void setApplicationUserId(User applicationUserId) {
+		this.applicationUserId = applicationUserId;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	/**
+	 * @return the insDate
+	 */
+	public Date getInsDate() {
+		return insDate;
+	}
+	/**
+	 * @param insDate the insDate to set
+	 */
+	public void setInsDate(Date insDate) {
+		this.insDate = insDate;
+	}
+	/**
+	 * @return the telNumber
+	 */
+	public String getTelNumber() {
+		return telNumber;
+	}
+	/**
+	 * @param telNumber the telNumber to set
+	 */
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the note
+	 */
+	public String getNote() {
+		return note;
+	}
+	/**
+	 * @param note the note to set
+	 */
+	public void setNote(String note) {
+		this.note = note;
+	}
 	
+	
+	
+		
 }
