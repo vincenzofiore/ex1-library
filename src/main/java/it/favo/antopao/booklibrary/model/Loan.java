7617c072ecfc083973883aee588de7524ee59ca4
4n70 <antonellafavorito@gmail.com>
2021-01-14 19:17:54 +0100

    BL-20 Loan entity close #20

---
+++ src/main/java/it/favo/antopao/booklibrary/model/Loan.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package it.favo.antopao.booklibrary.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Loan {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	private Volume volume;
+
+	private Date loanDate;
+	private Date returnDate;
+
+	@ManyToOne
+	private Member owner;
+
+	@ManyToOne
+	private Member borrower;
+
+	@Lob
+	private String notes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Volume getVolume() {
+		return volume;
+	}
+
+	public void setVolume(Volume volume) {
+		this.volume = volume;
+	}
+
+	public Date getLoanDate() {
+		return loanDate;
+	}
+
+	public void setLoanDate(Date loanDate) {
+		this.loanDate = loanDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+
+
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Member getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Member owner) {
+		this.owner = owner;
+	}
+
+	public Member getBorrower() {
+		return borrower;
+	}
+
+	public void setBorrower(Member borrower) {
+		this.borrower = borrower;
+	}
+
+}
