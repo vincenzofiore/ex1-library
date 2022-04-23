343cfe84f81f0ee7cbc4b0a6f70f3626f0cefb8f
4n70 <antonellafavorito@gmail.com>
2022-04-23 12:55:34 +0200

    BL-26 Fixed name fields

--- src/main/java/it/favo/antopao/booklibrary/model/Shelf.java
+++ src/main/java/it/favo/antopao/booklibrary/model/Shelf.java
@@ -4,7 +4,6 @@
 package it.favo.antopao.booklibrary.model;
 
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,70 +20,63 @@ import javax.persistence.OneToMany;
 @Entity
 public class Shelf {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String shelfName;
+  private Long shelfAddress;
+  @ManyToOne(cascade = CascadeType.ALL)
+  private Member owner;
+  @OneToMany(mappedBy = "shelf")
+  private Set<Volume> volume;
+  private Long notes;
 
-	private String shelf_name;
-	private Long shelf_address;
+  public Long getId() {
+    return id;
+  }
 
-	@ManyToOne(cascade = CascadeType.ALL)
-	private Member owner;
+  public void setId(Long id) {
+    this.id = id;
+  }
 
+  public String getShelfName() {
+    return shelfName;
+  }
 
-	@OneToMany(mappedBy = "shelf")
-	private Set<Volume> volume;
+  public void setShelfName(String shelfName) {
+    this.shelfName = shelfName;
+  }
 
-	private Long notes;
+  public Long getShelfAddress() {
+    return shelfAddress;
+  }
 
-	public Long getId() {
-		return id;
-	}
+  public void setShelfAddress(Long shelfAddress) {
+    this.shelfAddress = shelfAddress;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public Member getOwner() {
+    return owner;
+  }
 
-	public String getShelf_name() {
-		return shelf_name;
-	}
+  public void setOwner(Member owner) {
+    this.owner = owner;
+  }
 
-	public void setShelf_name(String shelf_name) {
-		this.shelf_name = shelf_name;
-	}
+  public Set<Volume> getVolume() {
+    return volume;
+  }
 
-	public Long getShelf_address() {
-		return shelf_address;
-	}
+  public void setVolume(Set<Volume> volume) {
+    this.volume = volume;
+  }
 
-	public void setShelf_address(Long shelf_address) {
-		this.shelf_address = shelf_address;
-	}
+  public Long getNotes() {
+    return notes;
+  }
 
-	public Long getNotes() {
-		return notes;
-	}
+  public void setNotes(Long notes) {
+    this.notes = notes;
+  }
 
-	public void setNotes(Long notes) {
-		this.notes = notes;
-	}
-
-	public Set<Volume> getVolume() {
-		return volume;
-	}
-
-	public void setVolume(Set<Volume> volume) {
-		this.volume = volume;
-	}
-
-	public Member getOwner() {
-		return owner;
-	}
-
-	public void setOwner(Member owner) {
-		this.owner = owner;
-	}
-
-	}
-
-
+}
